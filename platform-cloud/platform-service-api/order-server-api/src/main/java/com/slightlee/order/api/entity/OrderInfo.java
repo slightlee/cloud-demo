c7a66eaf067a68c51fb52a1a39bb37016ef4e433
morrow <limingwei@mentpeak.com>
2021-09-10 18:05:48 +0800

    refactor: 模块拆分

--- platform-cloud/platform-service-api/order-server-api/src/main/java/com/slightlee/order/api/entity/OrderInfo.java
+++ platform-cloud/platform-service-api/order-server-api/src/main/java/com/slightlee/order/api/entity/OrderInfo.java
@@ -1,4 +1,4 @@
-package com.slightlee.order.entity;
+package com.slightlee.order.api.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
